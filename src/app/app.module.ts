5c97bc56bb13b379c20b9e6cf603a28a028da7cc
sweetykumari <skumari@vertel.com.au>
2021-06-12 11:44:26 +0530

    sample project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [  //the declarations array contain AppComponent so that angular can locate its selector
+    AppComponent,
+    WelcomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, //so application runs correctly on the browser
+    HttpClientModule,
+    //Router module know about configured routes as we pass them into router module by calling the forRoot method.
+    RouterModule.forRoot([//this registers the router service provider,declares the router directives and exposes the configured routes.
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ]),
+    ProductModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //list our AppComponent as starting component for our application
 })
 export class AppModule { }
